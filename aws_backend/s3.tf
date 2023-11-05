c26504aecd9b54fab4fcaf447831e036ed22a31e
Turhan Mehmed <turhanmehmed3@turhans-mbp.home>
2023-11-05 17:12:09 +0000

    Added resource to s3.tf called Query Output Bucket

--- aws_backend/s3.tf
+++ aws_backend/s3.tf
@@ -5,3 +5,11 @@ resource "aws_s3_bucket" "achroma_db_landing" {
     Name = "Achroma DB Landing Bucket"
   }
 }
+
+resource "aws_s3_bucket" "achroma_db_queryoutput" {
+  bucket = "achroma-db-queryoutput-${data.aws_caller_identity.current.account_id}"
+
+  tags = {
+    Name = "Achroma DB Query Output Bucket"
+  }
+}
