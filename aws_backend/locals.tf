ba58055b234a0098ade784847fba3c39b495c004
JonathanHHenson <jonathan@henson.me.uk>
2023-11-11 16:20:27 +0000

    fixed locals.tf

--- aws_backend/locals.tf
+++ aws_backend/locals.tf
@@ -1,4 +1,4 @@
 locals {
   account_id = data.aws_caller_identity.current.account_id
-  resource_suffix = "${var.deployment_info.project_name}-${var.deployment_info.environment}-${account_id}"
+  resource_suffix = "${var.deployment_info.project_name}-${var.deployment_info.environment}-${local.account_id}"
 }
